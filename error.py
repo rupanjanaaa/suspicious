a65bb9a4927edb4a034f21875416540d812ee3e7
rupanjanaaa <162471824+rupanjanaaa@users.noreply.github.com>
2024-06-23 22:11:01 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,1 +1,48 @@
+Errors in Python Programming
 
+•Key error: Occurs when searching for a key in a dictionary that doesn't exist.
+
+Eg:- my_dict={"a": 1, "b":2}
+
+Print (my_dict ["e"])
+
+•Index error: Occurs when the index of a sequence is out of range.
+
+Eg:-my-list = [1,2,3] Print (my_list [5])
+
+* Indentation error : Occurs when there is an incorrect indentation.
+
+Eg:- def my_function(): Print ("Hello")
+
+* Attribute error: Raised on the attribute assignment or reference fails.
+
+Eg: x = 10
+
+Print (x, nonexistent_attribute)
+
+•Type error: Raised when a function or operation is applied to an object of an incorrect type.
+
+Eg:- = "Hello"
+
+Print (x+5)
+
+
+Name error: Raised when a variable is not found in the local or global scope.
+
+Eg:- Print (undefined_variable)
+
+•Value error:- occurs when function gets an argument of correct type but improper value.
+
+Eg:- float ("Hello")
+
+•System error: Raised when the interpreter detects interval error.
+
+E.g:- raise systemerror.
+
+* Syntax error: Raised by the parser when a syntax error is encountered.
+
+Eg:- Print "Hello"
+
+Zero Division Error: Raised when the second operand of a division or module operation is zero.
+
+Eg:-Print (64/0)
